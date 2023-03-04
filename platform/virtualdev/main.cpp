8015637e28ea5359b42a729382a6ae24af57e06a
Ee Chen <ee.ismyname@gmail.com>
2023-03-04 22:58:45 +1300

    #1: Pull RJCore execution into its own file

--- platform/virtualdev/main.cpp
+++ platform/virtualdev/main.cpp
@@ -51,20 +51,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-static void startRjCore(int fd)
-{
-    std::array<char, 64> buffer;
-    int bytesRead;
-
-    while ((bytesRead = read(fd, buffer.data(), buffer.size())) > 0)
-    {
-        // Just do an echo server for proof of concept
-        write(fd, buffer.data(), bytesRead);
-    }
-
-    if (bytesRead < 0)
-    {
-        std::cout << "Error while reading\n";
-    }
-}
