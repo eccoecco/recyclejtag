da8f3d1338de0159b8924c251a30109e4d477baf
Ee Chen <ee.ismyname@gmail.com>
2023-03-05 22:52:48 +1300

    #1: Change to epoll, and ignore sighup

--- platform/virtualdev/runcore.cpp
+++ platform/virtualdev/runcore.cpp
@@ -1,10 +1,15 @@
 #include <array>
 #include <ctime>
 #include <iostream>
+#include <memory>
 
+#include <signal.h>
+#include <sys/epoll.h>
 #include <unistd.h>
 
+#include "posixfile.h"
 #include "runcore.h"
+
 #include <rjcore/rjcore.h>
 
 struct PlatformData
@@ -12,7 +17,10 @@ struct PlatformData
     int fd;
 };
 
-static uint32_t PlatformImpl_CurrentUptime(void *)
+namespace
+{
+
+uint32_t PlatformImpl_CurrentUptime(void *)
 {
     struct timespec systemTime;
     clock_gettime(CLOCK_MONOTONIC, &systemTime);
@@ -20,17 +28,58 @@ static uint32_t PlatformImpl_CurrentUptime(void *)
     return systemTime.tv_sec + systemTime.tv_nsec / 1000000;
 }
 
-static void PlatformImpl_TransmitData(void *platformData, const char *buffer, size_t length)
+void PlatformImpl_TransmitData(void *platformData, const char *buffer, size_t length)
 {
     auto data = static_cast<PlatformData *>(platformData);
 
     write(data->fd, buffer, length);
 }
 
+std::unique_ptr<PosixFile> CreateEpollInstance(int fd)
+{
+    int epfd = epoll_create(1);
+    if (epfd < 0)
+    {
+        return nullptr;
+    }
+
+    auto ep = std::make_unique<PosixFile>(epfd);
+
+    struct epoll_event event
+    {
+        .events = EPOLLIN, .data = {},
+    };
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &event) < 0)
+    {
+        return nullptr;
+    }
+
+    return ep;
+}
+
+constexpr int epollTimeout_ms = 100;
+
+void ignoreSighup()
+{
+    // Mask out SIGHUP, so that the remote pseudo-terminal closing doesn't interrupt this
+    sigset_t signals;
+    sigemptyset(&signals);
+    sigaddset(&signals, SIGHUP);
+    sigprocmask(SIG_BLOCK, &signals, NULL);
+}
+
+} // namespace
+
 void startRjCore(int fd)
 {
-    std::array<char, 64> buffer;
-    int bytesRead;
+    ignoreSighup();
+
+    auto ep = CreateEpollInstance(fd);
+    if (!ep)
+    {
+        std::cout << "Failed to create epoll instance\n";
+        return;
+    }
 
     RJCoreState rjcore;
     RJCorePlatform platform{
@@ -43,17 +92,42 @@ void startRjCore(int fd)
 
     RJCore_Init(&rjcore, &platform, &platformData);
 
-    // TODO: Swap to poll, and implement timeouts
-    while ((bytesRead = read(fd, buffer.data(), buffer.size())) > 0)
+    while (true)
     {
-        if (bytesRead > 0)
-        {
-            RJCore_NotifyDataReceived(&rjcore, buffer.data(), bytesRead);
-        }
-    }
+        struct epoll_event event;
+        int descriptorsReady;
+        std::array<char, 64> buffer;
+        int bytesRead;
 
-    if (bytesRead < 0)
-    {
-        std::cout << "Error while reading\n";
+        descriptorsReady = epoll_wait(ep->Descriptor(), &event, 1, epollTimeout_ms);
+
+        if (descriptorsReady < 0)
+        {
+            std::cout << "Epoll error\n";
+            break;
+        }
+
+        if (descriptorsReady == 0)
+        {
+            bytesRead = 0;
+        }
+        else
+        {
+            bytesRead = read(fd, buffer.data(), buffer.size());
+            if (bytesRead == 0)
+            {
+                // SIGHUP - other end has closed the connection
+                std::cout << "Remote end closed connection\n";
+                break;
+            }
+        }
+
+        if (bytesRead < 0)
+        {
+            std::cout << "Error while reading\n";
+            break;
+        }
+
+        RJCore_NotifyDataReceived(&rjcore, buffer.data(), bytesRead);
     }
 }
