64eba9551c64160f71a1c25bce5307c0ab5cd366
Ee Chen <ee.ismyname@gmail.com>
2023-03-16 23:34:34 +1300

    #3: Simple echo server where reply = (in + 1) for testing

--- platform/lpc845-brk/main.cpp
+++ platform/lpc845-brk/main.cpp
@@ -1,10 +1,39 @@
 #include <cstdint>
 
+#include <LPC845.h>
+
 #include "init.h"
+
+static void serialEcho();
 
 int main()
 {
     Init::InitSystem();
 
+    // TODO: Turn serial port into interrupt based
+
+    serialEcho();
+
     return 0;
+}
+
+void serialEcho()
+{
+    while (1)
+    {
+        uint32_t status = USART0->STAT;
+        if ((status & USART_STAT_RXRDY(1)) != 0)
+        {
+            USART0->STAT = UINT32_MAX;
+
+            // Data ready!
+            // If using parity for extra data, then use RXDATSTAT instead
+            uint32_t rxData = USART0->RXDAT;
+
+            ++rxData;
+            rxData &= 0xFF;
+
+            USART0->TXDAT = rxData;
+        }
+    }
 }
