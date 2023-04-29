a7eef31dded51260334df4d9c90866b0804af0f6
Ee Chen <ee.ismyname@gmail.com>
2023-04-29 23:03:21 +1200

    #3: Wire up GPIO mode

--- platform/lpc845-brk/main.cpp
+++ platform/lpc845-brk/main.cpp
@@ -7,71 +7,129 @@
 #include "systick.h"
 #include "uart.h"
 
-static void serialEcho();
+#include <rjcore/rjcore.h>
+
+static void startRjCore();
 
 int main()
 {
     Init::InitSystem();
 
-    // TODO: RJCore
-
-    serialEcho();
+    startRjCore();
 
     return 0;
 }
 
-void serialEcho()
+namespace
 {
-    constexpr uint32_t testDelay_ms = 1000;
+namespace PlatformImpl
+{
 
-    uint32_t lastSend_ms = SystemTick::CurrentTick();
-    uint8_t lastSentChar = '0';
+uint32_t CurrentUptime(void *)
+{
+    return SystemTick::CurrentTick();
+}
 
-    while (1)
+void TransmitData(void *, const void *buffer, size_t length)
+{
+    auto data = static_cast<const uint8_t *>(buffer);
+
+    for (; length > 0; ++data, --length)
     {
-        auto currentTick = SystemTick::CurrentTick();
-        if ((currentTick - lastSend_ms) >= testDelay_ms)
-        {
-            lastSend_ms += testDelay_ms;
+        Uart::WriteCharacter(*data);
+    }
+}
 
-            Uart::WriteCharacter(lastSentChar);
+bool SetSerialMode(void *, enum RJCoreSerialMode)
+{
+    // Does not support 1Mbps - only 115,200
+    return false;
+}
 
-            if (lastSentChar == '9')
-            {
-                lastSentChar = '0';
-            }
-            else
-            {
-                ++lastSentChar;
-            }
-        }
+// Implementation that sets the port mode
+bool SetPortMode(void *, enum RJCorePortMode portMode)
+{
+    switch (portMode)
+    {
+    case RJCorePortMode_HighImpedance:
+        // TODO: Write to DIRCLR for the GPIO pins
+        break;
+    case RJCorePortMode_PushPull:
+        // TODO: Write to DIRSET for the GPIO pins
+        break;
+    default:
+        // Don't support open drain mode (or other modes)
+        // Only support high impedance and push-pull
+        return false;
+    }
 
+    return true;
+}
+
+bool SetFeature(void *, enum RJCoreFeature, int)
+{
+    // TODO: Set feature
+    return true;
+}
+
+void ReadVoltages(void *, uint16_t *values)
+{
+    values[0] = 0;
+    values[1] = 0;
+    values[2] = 0;
+    values[3] = 0;
+}
+
+void NewTapShift(void *, int totalBitsToShift)
+{
+    // Since only doing GPIO for now, no need to track total bits to shift
+    (void)totalBitsToShift;
+}
+
+// Generate a clock cycle (TCK low, TDI = tdi, TMS = tms, pause, TCK high, read TDO)
+// Returns 0 if TDO low, 1 if TDO high
+int TapShiftGPIOClockCycle(void *, int tdi, int tms)
+{
+    Gpio::SetState<Gpio::Mapping::JtagTck>(false);
+    Gpio::SetState<Gpio::Mapping::JtagTdi>((tdi != 0));
+    Gpio::SetState<Gpio::Mapping::JtagTms>((tms != 0));
+
+    Gpio::SetState<Gpio::Mapping::JtagTck>(true);
+    return Gpio::GetState<Gpio::Mapping::JtagTdo>();
+}
+
+} // namespace PlatformImpl
+} // namespace
+
+void startRjCore()
+{
+    RJCoreHandle rjcoreHandle;
+    RJCorePlatform platform{
+        .tapShiftMode = RJCoreTapShiftMode_GPIO,
+        .currentUptime = PlatformImpl::CurrentUptime,
+        .transmitData = PlatformImpl::TransmitData,
+        .setSerialMode = PlatformImpl::SetSerialMode,
+        .setPortMode = PlatformImpl::SetPortMode,
+        .setFeature = PlatformImpl::SetFeature,
+        .readVoltages = PlatformImpl::ReadVoltages,
+        .newTapShift = PlatformImpl::NewTapShift,
+        .tapShiftGPIO = PlatformImpl::TapShiftGPIOClockCycle,
+        .tapShiftPacket = nullptr, // TODO: Use packet mode when doing hardware SPI?
+        .tapShiftCustom = nullptr, // TODO: Or maybe use custom mode?
+    };
+
+    RJCore_Init(&rjcoreHandle, &platform, nullptr);
+
+    while (true)
+    {
         int readCharacter = Uart::ReadCharacter();
         if (readCharacter < 0)
         {
             continue;
         }
 
-        Uart::WriteCharacter(readCharacter);
+        auto value = static_cast<uint8_t>(readCharacter);
 
-        switch (readCharacter)
-        {
-        case 'r':
-        case 'R':
-            Gpio::SetState<Gpio::Mapping::DebugRed>(readCharacter == 'R');
-            break;
-        case 'g':
-        case 'G':
-            Gpio::SetState<Gpio::Mapping::DebugGreen>(readCharacter == 'G');
-            break;
-        case 'b':
-        case 'B':
-            Gpio::SetState<Gpio::Mapping::DebugBlue>(readCharacter == 'B');
-            break;
-        case 'i':
-        case 'I':
-            Uart::WriteCharacter(Gpio::GetState<Gpio::Mapping::JtagTdo>() ? 'H' : 'L');
-            break;
-        }
+        RJCore_NotifyDataReceived(&rjcoreHandle, &value, 1);
     }
 }
