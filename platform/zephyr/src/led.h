49e5f5b4a94fbc9bd700de6ff8f821c08c7232d6
Ee Chen <ee.ismyname@gmail.com>
2023-09-30 23:31:18 +1300

    #18 - Wiring up LED blinking

--- platform/zephyr/src/led.h
+++ platform/zephyr/src/led.h
@@ -13,19 +13,20 @@ extern "C"
 
 void led_init(void);
 
+// These enums are the blink pattern
 enum led_normal_status
 {
-    LED_IDLE,
-    LED_ACTIVE,
+    LED_NORMAL_IDLE = 0x1F000, //!< Medium time on, long time off
+    LED_NORMAL_ACTIVE = 0x3E0, //!< Medium time on, medium time off
 };
 
 void led_update_status(enum led_normal_status status);
 
 enum led_error_status
 {
-    LED_ERROR_NONE,
-    LED_ERROR_USB_FAULT,
-    LED_ERROR_HARDWARE_FAULT,
+    LED_ERROR_NONE = 0,                 //!< No error - no blink
+    LED_ERROR_USB_FAULT = 0x14000,      //!< Two fast blinks
+    LED_ERROR_HARDWARE_FAULT = 0x15000, //!< Three fast blinks
 };
 
 void led_update_error(enum led_error_status status);
